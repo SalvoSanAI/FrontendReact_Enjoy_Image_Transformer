f343a2ba520024b1c5d3c44e34412172c4a0f809
SalvoSan <salvo.san@libero.it>
2026-06-17 19:24:54 +0200

    Add redirectUri

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
     auth: {
         clientId: "94d43f1e-376a-4787-881e-eeeaee29e023", // ID dell'App Registration del Frontend (Passo 4)
         authority: "https://login.microsoftonline.com/4ebd9f8c-58ab-4a1f-8249-f2e2313a8b54", // Il tuo Directory (tenant) ID
-        redirectUri: "http://localhost:3000", // Dove gira React in locale
+        redirectUri: "https://polite-field-0e522a110.7.azurestaticapps.net", // Dove gira React in locale
     },
     cache: {
         cacheLocation: "sessionStorage", // Salva il login nella sessione del browser
